a5850656c0dc2b74b45ed7b37905900e45f1380b
Samir Medjdoub <contact@codeconcept.fr>
2017-03-13 16:32:31 +0100

    adding nodemon and custom npm scripts

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World !');
+    res.status(200).send('Hello World with npm custom scripts');
 });
 
 app.listen(port, () => {
