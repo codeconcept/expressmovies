b0b00d17a64ee1338678915acd212ddeed525ff3
Samir Medjdoub <contact@codeconcept.fr>
2017-03-13 16:54:55 +0100

    order of routes

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World with npm custom scripts');
 });
 
+app.get('/movies', (req, res) => {
+    res.send('Bientôt des <b>films</b> ici même.');
+});
+
+app.get('/movies/add', (req, res) => {
+    res.send('prochainement, un formulaire d\'ajout ici');
+});
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`film numéro ${id}.`);
+});
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
